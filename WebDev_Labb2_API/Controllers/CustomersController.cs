3aed9eef8b1d9379447748f804943f1d8d79e0ca
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-03-19 11:22:39 +0100

    Add customer management methods to CustomersController
    Implemented HTTP POST method for adding new customers,
    including checks for existing email and username.
    Added HTTP PATCH method for updating customer details
    based on specified fields. Both methods return appropriate
    success or error messages.

--- WebDev_Labb2_API/Controllers/CustomersController.cs
+++ WebDev_Labb2_API/Controllers/CustomersController.cs
@@ -49,5 +49,97 @@ namespace WebDev_Labb2_API.Controllers
                 return null;
             }
         }
+
+        [HttpPost(Name = "AddCustomer")]
+        public string Post(string username, string firstname, string lastname, string email, string mobile_number, string city, string country, string postcode, string street)
+        {
+            try
+            {
+                using (var db = new DBContext())
+                {
+                    if (db.Customers.Where(c => c.email == email).FirstOrDefault() != null)
+                    {
+                        return "Customer already exists.";
+                    }
+                    if (db.Customers.Where(c => c.username == username).FirstOrDefault() != null)
+                    {
+                        return "Username already exists.";
+                    }
+                    var deliveryadress = new DeliveryAddress
+                    {
+                        street = street,
+                        post_code = postcode,
+                        city = city,
+                        country = country
+                    };
+                    var customer = new Customers
+                    {
+                        username = username,
+                        userlevel = "customer",
+                        firstname = firstname,
+                        lastname = lastname,
+                        email = email,
+                        mobile_number = mobile_number,
+                        delivery_adress = deliveryadress
+                    };
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                    return $"Customer {firstname} {lastname} added sucessfully.";
+                }
+            }
+            catch
+            {
+                Console.Write("Error");
+                return null;
+            }
+        }
+
+        [HttpPatch("{email}", Name = "UpdateCustomer")]
+        public string Patch(string email, string field, string value)
+        {
+            try
+            {
+                using (var db = new DBContext())
+                {
+                    var customer = db.Customers.Where(c => c.email == email).FirstOrDefault();
+                    if (customer != null)
+                    {
+                        switch (field)
+                        {
+                            case "username":
+                                customer.username = value;
+                                break;
+                            case "userlevel":
+                                customer.userlevel = value;
+                                break;
+                            case "firstname":
+                                customer.firstname = value;
+                                break;
+                            case "lastname":
+                                customer.lastname = value;
+                                break;
+                            case "email":
+                                customer.email = value;
+                                break;
+                            case "mobile_number":
+                                customer.mobile_number = value;
+                                break;
+                            default:
+                                return "Field not found";
+                        }
+                        db.SaveChanges();
+
+                        return $"Customer {field} updated to {value} sucessfully.";
+                    }
+
+                    return "Customer not found";
+                }
+            }
+            catch
+            {
+                Console.Write("Error");
+                return null;
+            }
+        }
     }
 }
